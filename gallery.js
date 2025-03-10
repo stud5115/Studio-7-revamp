f9456118309def3e60fcb3f29cb11b17e9e9d72d
Njabulo Kubheka <142051255+stud5115@users.noreply.github.com>
2025-03-11 00:19:06 +0200

    Add files via upload

---
+++ gallery.js
@@ -0,0 +1,29 @@
+let slideIndex = 1;
+let reviewIndex = 0;
+
+function openModal() {
+    document.getElementById('myModal').style.display = "block";
+}
+
+function closeModal() {
+    document.getElementById('myModal').style.display = "none";
+}
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[slideIndex-1].style.display = "block";
+}
